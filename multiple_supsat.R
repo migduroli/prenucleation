c359f7ea63cc968053d6a4da4b3cc6c6fedd6879
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2021-11-08 18:33:54 +0100

    refac: add new functionality to make plots more visually attractive and to plot the p_eq

--- multiple_supsat.R
+++ multiple_supsat.R
@@ -6,8 +6,10 @@ source("utils/graphs.R")
 supsat_values <- seq(1.75, 3.25, length = 10)
 
 limits <- list()
+
 for (s in supsat_values)
 {
-  m_bulk <- initialize(beta = b_beta, supsat = s, verbose = T)
-  limits <- plot_potentials(bulk = m_bulk, limits = limits)
+  m_bulk <- initialize(beta = b_beta, supsat = s, verbose = F)
+  # limits <- plot_potentials(bulk = m_bulk, limits = limits)
+  limits <- plot_probability(bulk = m_bulk, limits = limits)
 }
--- utils/graphs.R
+++ utils/graphs.R
@@ -1,20 +1,34 @@
 library(shape)
 
+setup_margins <- function(left = 4.5, right = 2.5, bottom = 6, top = 4) {
+  par(mar=c(bottom, left, top, right)+.1)
+}
+
 plot_radius <- function(bulk) {
   radii <- r_path(x = bulk$rho, rho_v = bulk$rho_vap, rho_l = bulk$rho_liq)
 
-  plot(bulk$rho, radii, "o", ylim=c(0,8))
-  abline(h=bulk$r_min, lty=2)
+  setup_margins()
+  plot(bulk$rho, radii,
+       type="l",
+       lwd=2,
+       ylim=c(0,8),
+       xlab=expression(rho[0]),
+       ylab=expression(paste("R(", rho[0],")")),
+       cex.lab=1.25
+  )
+  # abline(h=bulk$r_min, lty=2)
   abline(v=bulk$rho_liq, lty=2)
   abline(v=bulk$rho_vap, lty=2)
 }
 
 plot_omega <- function(bulk)
 {
-  Omega <- W(x = bulk$rho,
-             rho_v = bulk$rho_vap,
-             rho_l = bulk$rho_liq,
-             beta = bulk$beta)
+  Omega <- W(
+    x = bulk$rho,
+    rho_v = bulk$rho_vap,
+    rho_l = bulk$rho_liq,
+    beta = bulk$beta
+  )
 
   cc_idx <- which.max(Omega)
   o_cc <- Omega[cc_idx]
@@ -24,7 +38,12 @@ plot_omega <- function(bulk)
        y = Omega,
        xlim=c(bulk$rho_vap, 1.1 * r_cc),
        ylim=c(-1, 1.1 * o_cc ),
-       type="o")
+       xlab=expression(rho[0]),
+       ylab=expression(beta*Omega),
+       cex.lab=1.25,
+       type="l",
+       lwd=3
+  )
 
   points(r_cc, o_cc, col="red", pch=16)
   abline(h = o_cc, lty = 2, col="grey")
@@ -104,19 +123,20 @@ plot_panel <- function(n, bulk_obj, omega_obj, lims_obj, is_first) {
       ylab <- expression(paste(beta, Omega[eff]))
     }
 
+    setup_margins()
     plot(
       x = bulk_obj$rho,
       y = omega_obj$omega,
       xlim = lims_obj$xlim,
       ylim = lims_obj$ylim,
-      xlab = expression(rho),
+      xlab = expression(rho[0]),
       ylab = ylab,
       type = "l",
       lwd = 2,
       cex.lab=1.25
     )
     abline(h=0)
-    abline(h=0.5,col=3 ,lwd=2)
+    # abline(h=1,col=3 ,lwd=2)
 
     Arrows(
       omega_obj$r_c * 1.075,
@@ -201,4 +221,90 @@ plot_potentials <- function(bulk, limits) {
     ylim=lims$ylim,
     xlim_eff=lims_eff$xlim,
     ylim_eff=lims_eff$ylim))
+}
+
+
+compute_p_eq <- function(omega_obj, bulk) {
+
+  cc_eff_idx <- which.max(omega_obj$omega)
+
+  mask <- 1:(5+cc_eff_idx)
+  e <- exp(-omega_obj$omega[mask])
+
+  e_norm <- sum(diff(bulk$rho[mask]) * e[1:(length(e)-1)], na.rm = T)
+  e <- e/max(e_norm)
+
+  p_obj <- list(
+    rho=bulk$rho[mask],
+    prob=e,
+    r_c=bulk$rho[cc_eff_idx],
+    p_c=e[cc_eff_idx]
+  )
+
+  return(p_obj)
+}
+
+plot_prob_panel <- function(is_first, p_eq_obj, limits) {
+
+  if (is_first) {
+    limits <- list(
+      ylim=c(0, 10)
+    )
+
+    setup_margins()
+    plot(
+      x=p_eq_obj$rho,
+      y=p_eq_obj$prob,
+      ylim=limits$ylim,
+      type="l",
+      lwd=2,
+      xlab=expression(rho[0]),
+      ylab=expression(P[eq]),
+      cex.lab=1.25
+    )
+    abline(h=0, lty=2)
+    # abline(h=exp(-1), lwd=2, col=3)
+
+    Arrows(
+      p_eq_obj$r_c,
+      0,
+      p_eq_obj$r_c,
+      4,
+      arr.type = "curved",
+      col = "red",
+      lwd = 2
+    )
+
+  } else {
+    lines(p_eq_obj$rho, p_eq_obj$prob, lty=2)
+  }
+
+  points(
+    x=p_eq_obj$r_c,
+    y=p_eq_obj$p_c,
+    col=2,
+    pch=16
+  )
+
+  return(limits)
+}
+
+plot_probability <- function(bulk, limits) {
+  is_first <- is.list(limits) & length(limits) == 0
+
+  if (is_first) { par(mfrow=c(1,1)) }
+
+  w_objs <- compute_potentials(bulk = bulk)
+
+  o_eff_obj <- w_objs$w_eff
+
+  p_eq <- compute_p_eq(omega_obj = o_eff_obj, bulk = bulk)
+
+  limits <- plot_prob_panel(
+    is_first = is_first,
+    p_eq_obj = p_eq,
+    limits = limits
+  )
+
+  return(limits)
 }
